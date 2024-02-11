e2f70e96330e32b65da58dfb28c9c463f73a5cd4
insertokname <insertokname@gmail.com>
2024-02-12 01:09:27 +0200

    added a few slides

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from manim_slides import Slide
 
 TITLE_SIZE = 75
 SUB_SIZE = 40
-APPEAR_SPEED = 1.2
+APPEAR_SPEED = 1.25
 DISAPPEAR_SPEED=0.5
+
+TOP_POS = UP*2.8
 
 def slide_1(slide):
     Title = Text("Standardul IEEE 754", font_size=TITLE_SIZE).shift(UP*0.5)
@@ -20,8 +22,8 @@ def slide_1(slide):
     slide.play(Wait(0.15))
 
 def slide_2(slide):
-    Title = Text("Fun fact", font_size=TITLE_SIZE).shift(UP*0.5)
-    Subtitle = Text('Știați că numarul "0.3" nu există?', font_size=SUB_SIZE,slant="ITALIC").shift(DOWN*0.5)
+    Title = Text('Știați că numarul "0.3"', font_size=TITLE_SIZE).shift(UP*0.5)
+    Subtitle = Text('nu există?', font_size=TITLE_SIZE).shift(DOWN*0.5)
     
     slide.play(Write(Title),Write(Subtitle),run_time=APPEAR_SPEED)
     slide.play(Wait(0.1))
@@ -44,13 +46,67 @@ def slide_3(slide):
 
 def slide_4(slide):
 
-    Title = Text("Despre aceasta prezentare", font_size=TITLE_SIZE)
+    Title = Text("Despre aceasta prezentare", font_size=TITLE_SIZE*0.8)
     
     slide.play(Write(Title),run_time=APPEAR_SPEED)
     slide.play(Wait(0.1))
     slide.next_slide()
 
     slide.play(Uncreate(Title),run_time=DISAPPEAR_SPEED)
+    slide.play(Wait(0.15))
+
+def slide_5(slide):
+
+    Title = Text("S-o luam cu inceputul", font_size=TITLE_SIZE)
+    
+    slide.play(Write(Title),run_time=APPEAR_SPEED)
+    slide.play(Wait(0.1))
+
+    slide.next_slide()
+    slide.play(Uncreate(Title),run_time=DISAPPEAR_SPEED)
+    slide.play(Wait(0.15))
+
+def slide_6(slide):
+
+    Title = Text("Sistemul binar", font_size=TITLE_SIZE)
+    
+    slide.play(Write(Title),run_time=APPEAR_SPEED)
+    slide.play(Wait(0.1))
+
+    slide.next_slide()
+    slide.play(Uncreate(Title),run_time=DISAPPEAR_SPEED)
+    slide.play(Wait(0.15))
+
+def slide_7(slide):
+
+    Title = Text("Cum functioneaza?", font_size=TITLE_SIZE*0.95).shift(TOP_POS)
+    
+    slide.play(Write(Title),run_time=APPEAR_SPEED)
+    slide.play(Wait(0.1))
+
+    slide.next_slide()
+    dec_maths =Text("123 = 100+20+3", font_size=SUB_SIZE,slant="ITALIC").to_edge(LEFT).shift(UP*1)
+    slide.play(Write(dec_maths),run_time=APPEAR_SPEED,)
+
+    slide.next_slide()
+    dec_table = Table(
+        [["100", "10","1"],
+        ["1", "2", "3"]]).to_edge(RIGHT).shift(UP*0.9).shift(LEFT*0.74)
+    slide.play(DrawBorderThenFill(dec_table),run_time=APPEAR_SPEED)
+
+    slide.next_slide()
+    bin_maths =Text("1101= 8+4+0+1",font_size=SUB_SIZE,slant="ITALIC").to_edge(LEFT).shift(DOWN*1.85)
+    sub_bin_maths =Text("(13)",font_size=SUB_SIZE,slant="ITALIC").to_edge(LEFT).shift(DOWN*1.95)
+    slide.play(Write(sub_bin_maths),Write(bin_maths),run_time=APPEAR_SPEED)
+
+    slide.next_slide()
+    bin_table = Table(
+        [["8","4", "2","1"],
+        ["1", "1", "0", "1"]]).to_edge(RIGHT).shift(DOWN*2).shift(LEFT*0.24)
+    slide.play(DrawBorderThenFill(bin_table),run_time = APPEAR_SPEED)
+
+    slide.next_slide()
+    slide.play(Uncreate(sub_bin_maths),Uncreate(Title),Uncreate(bin_maths),Uncreate(dec_maths),Uncreate(bin_table),Uncreate(dec_table),run_time=DISAPPEAR_SPEED)
     slide.play(Wait(0.15))
 
 class BasicExample(Slide):
@@ -60,3 +116,6 @@ class BasicExample(Slide):
         slide_2(self)
         slide_3(self)
         slide_4(self)
+        slide_5(self)
+        slide_6(self)
+        slide_7(self)
